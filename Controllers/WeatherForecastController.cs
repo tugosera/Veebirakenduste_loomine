cdf9303e3e06863814ea5b8bb4825472965dc9dd
opilane <opilane@HP-CZC2349HTR>
2025-09-30 14:49:20 +0300

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Veebirakenduste_loomine;
+
+namespace veeb.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherController> _logger;
+
+        public WeatherController(ILogger<WeatherController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Models/Toode.cs
@@ -0,0 +1,36 @@
+﻿namespace Veebirakenduste_loomine.Models
+{
+    public class Toode
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+
+        public Toode(int id, string name, double price, bool isActive)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            IsActive = isActive;
+        }
+    }
+
+    public class Kasutaja
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public Kasutaja(int id, string userName, string password, string name, string lastName)
+        {
+            Id = id;
+            UserName = userName;
+            Password = password;
+            Name = name;
+            LastName = lastName;
+        }
+    }
+}
